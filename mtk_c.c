c68da59b075a45b1815fdb15f008621f96ec3eff
Okapi Dog <150521381+Okapi-dog@users.noreply.github.com>
2024-11-27 17:12:13 +0900

    Update mtk_c.c

--- mtk_c.c
+++ mtk_c.c
@@ -28,3 +28,24 @@ typedef struct {
 
 STACK_TYPE	stacks[NUMTASK];
 
+void init_kernel(){
+
+	//TCV配列の初期化
+	for(i=0; i< NUMTASK;i++){
+		task_tab[i].task_addr	=NULL
+		task_tab[i].stack_ptr	=NULL
+		task_tab[i].priority 	=NULL
+		task_tab[i].status	=NULL
+	}
+	//readyキューの初期化
+	TASK_ID_TYPE=NULLTASKID
+	//[TODO]PVシステムコールの割り込み処理ルーチン(pv_handler)をTRAP1の割り込みベクタに登録する
+	//セマフォの値を初期化
+	for(i=0; i< NUMSEMAPHORE;i++){
+		semaphore[i].count	=NULL
+		semaphore[i].nst	=NULL
+		semaphore[i].task_list 	=NULL
+	}
+
+
+}
