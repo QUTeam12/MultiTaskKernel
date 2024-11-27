26ed14dbf36fab8d60d863c280e31182e6e54c34
shutouyusei <syutou.yusei.061@s.kyushu-u.ac.jp>
2024-11-27 17:05:03 +0900

    [fix]スタックポインタの扱いを変更

--- mtk_asm.s
+++ mtk_asm.s
@@ -35,10 +35,10 @@ V:
 ** in -> d0 (p => 0 , v => 1) 
 ** in -> d1 (semaphoreId)
 pv_handler:
-	movem.l %d0-%d1/%a7, -(%sp) /* 実行タスクのレジスタ退避 */
+	movem.l %d0-%d1, -(%sp) /* 実行タスクのレジスタ退避 */
 	move.w %sr,-(%sp)	
 	move.w #0x2700, %sr	/*割り込み禁止*/	
-	move.l %d1,-(%a7) /*引数を追加*/
+	move.l %d1,-(%sp) /*引数を追加*/
 	cmpi.l #0, %d0	
 	beq call_p_body
 	bra call_v_body
@@ -50,6 +50,7 @@ call_v_body:
 	bra pv_handler_end
 pv_handler_end:
 	/* TODO:引数除去必要か要検証 */
+	add.l #4, %sp
 	move.w (%sp)+, %sr	/*走行レベル回復*/
-	movem.l (%sp)+, %d0-%d1/%a7/*　レジスタ復帰*/
+	movem.l (%sp)+, %d0-%d1/*　レジスタ復帰*/
 	rte
