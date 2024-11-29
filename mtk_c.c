3010c04243f8383f6a999aab6669b95fecd4b229
Kaito <shigyo.kaito.437@s.kyushu-u.ac.jp>
2024-11-29 16:58:36 +0900

    [add]set_stack

--- mtk_c.c
+++ mtk_c.c
@@ -17,7 +17,7 @@ typedef struct {
 	void *stack_ptr; // タスク毎にユーザスタックとスーパーバイザスタックを分けるスタックのポインタ
 	int priority; // 優先度。今回は使わない。
 	int status; // TCBの使用状態。今回は使わない。
-	TASK_ID_TYPE next; // セマフォかreadyキューの次のタスクID
+	TASK_ID_TYPE next; // セマフォかreadyキューの次のタスクID。詳細はp33。
 } TCB_TYPE;
 
 TCB_TYPE	task_tab[NUMTASK+1]; // task_tab[1]からID=1のタスクを割り振る
@@ -29,24 +29,47 @@ typedef struct {
 
 STACK_TYPE	stacks[NUMTASK]; // stacks[0]からID=1のタスクを割り振る
 
+TASK_ID_TYPE curr_task; // 現在実行中のタスクのID
+TASK_ID_TYPE new_task; // 現在登録作業中のタスクのID
+TASK_ID_TYPE ready; // 実行待ちタスクキューの先頭タスクのID
+
 void init_kernel() {
 	// TCB配列の初期化
 	for(int i=1; i <= NUMTASK; i++){
 		task_tab[i].task_addr = NULL;
 		task_tab[i].stack_ptr = NULL;
 		task_tab[i].priority = UNDEFINED;
-		task_tab[i].status = UNDEFINED;
+		task_tab[i].status = TCB_UNDEFINED;
 		task_tab[i].next = NULLTASKID;
 	}
 	// readyキューの初期化
-	TASK_ID_TYPE ready = NULLTASKID;
-	// TODO: PVシステムコールの割り込み処理ルーチン(pv_handler)をTRAP1の割り込みベクタに登録する
+	ready = NULLTASKID;
+	// PVシステムコールの割り込み処理ルーチン(pv_handler)をTRAP1の割り込みベクタに登録
 	*(void(**) ())0x084 = pv_handler; // TODO: trap1のアドレスがあってるかわからない
-	// セマフォの値の初期化
+	// セマフォのフィールド群の初期化
 	for(int i=0; i< NUMSEMAPHORE;i++){
 		semaphore[i].count = 1; // TODO: 初期のリソースアクセス状況は1だが正直わからん
 		semaphore[i].nst = UNDEFINED;// TODO: nstの意味がそもそもわからない
 		semaphore[i].task_list 	= NULLTASKID;
 	}
+}
+
+void set_task(void (*user_task_func)()) {
+	for(int i=1; i <= NUMTASK; i++){
+		if (task_tab[i].task_addr == NULL) { 
+			// TCB配列の空きスロットにユーザタスク関数を定義
+			new_task = i;
+			task_tab[i].task_addr = user_task_func;
+			task_tab[i].status = TCB_ACTIVE;
+			task_tab[i].stack_ptr = init_stack(new_task);
+			ready = new_task;
+			return;
+		}
+	}
+}
+
+void* init_stack(TASK_ID_TYPE id) {
+    void *ssp;
+    return ssp;
 }
 
