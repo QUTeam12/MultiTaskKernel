e203e466f7079eb79fffbe3d8557de8c707afd42
Okapi-dog <150521381+Okapi-dog@users.noreply.github.com>
2024-12-04 15:40:24 +0900

    step3の途中

--- mtk_asm.s
+++ mtk_asm.s
@@ -4,13 +4,14 @@
 .global pv_handler
 .global hard_clock
 .global init_timer
+.global swtch
 .extern p_body
 .extern v_body
 .extern curr_task
 .extern ready
 .extern addq
 .extern sched
-.extern swtch
+.extern stacks
 
 .section .text
 .even
@@ -90,3 +91,30 @@ init_timer:
 	move.l  #hard_clock, %d2
 	trap #0
 	rts
+
+******************************
+**swtch
+******************************
+swtch:
+	move.w 	%sr,-(%sp)	/*SRの値をスタックに積む*/
+	movem.l %d0-%d7/%a0-%a6 -(%sp)
+	move.w	%a7,-(%sp)	/*USPの値をスタックに積む*/
+	movem.l %a0, -(%sp)     | 使用レジスタの退避[TODO]レジスタ追加
+	move.l	task_tab, %d0	| TCB配列の先頭アドレス
+	move.l	curr_task, %d1	| 現在のタスクID
+	mulu.w	#10, %d1
+	add.l	#2, %d1		| TCBの先頭から4バイト目にSSPが格納されているため4を加算
+	add.l	%d1, %d0	| curr_taskが指すTCBのアドレス計算
+	move.l	%d0, %d3	|上までで計算したTCBのSSPの格納先アドレス
+	
+        move.l  stacks, %d0   	| stacks配列の先頭アドレス
+        move.l  curr_task, %d1  | 現在のタスクID
+	sub.l	#1,%d1		| stacks配列では0スタートだが、taskIDは1スタートなので、変換
+	/*ここからしばらくワード型で計算taskIDは多くても10とかだし、STKSIZEは8000なので、二倍して16000これを10倍してもワード型をあふれることはないだろう*/
+	mulu.w	#STKSIZE, %d1	|ここまでの計算で(taskID -1) * STKSIZE(char(1Byte)*STKSIZE) / 2(2Byteで1アドレス) * 2(構造体内の配列の個数)をした
+	
+        add.l   #half_STKSIZE, %d1         | stacksの先頭からSTKSIZE Byte目にSSPが格納されているためSTKSIZE/2を加算
+        add.l   %d1, %d0        | curr_taskのstacksのSSPのアドレスをd0に保存
+	
+	|TODO:stacksの番地計算とTCBにSSPを記録することとMULUの変更の適応
+	move.l	(%a0), %sp	| TCBにSSPを記録		
--- mtk_c.h
+++ mtk_c.h
@@ -1,6 +1,6 @@
 #define NULLTASKID 0 // キューの終端
 #define NUMSEMAPHORE 3 // TODO: セマフォの数がわからないから暫定の値
 #define NUMTASK 5 // 最大タスク数
-#define STKSIZE 8000 // スタックサイズ
+#define STKSIZE 8000 // スタックサイズ(変更するときは、equのdefineも変更すること)
 #define UNDEFINED 0 // int型フィールドの初期化用
 
